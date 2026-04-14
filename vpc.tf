4059da57a79dbbd0616abb544f5b6f56b6ddf213
Karan Patel <patel_karan_cool@hotmail.com>
2026-04-14 14:31:16 -0400

    Add VPC module configuration in vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "devops-capstone-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true # Saves cost for personal projects
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+    # Allows EKS to discover subnets for internal load balancers
+    "kubernetes.io/cluster/devops-capstone-cluster" = "shared"
+  }
+}
