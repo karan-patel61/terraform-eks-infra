774f7e27e37a02214c230268af6fe8a837952697
Karan Patel <patel_karan_cool@hotmail.com>
2026-04-14 14:30:20 -0400

    Configure EKS module in main.tf
    Add EKS module configuration for devops-capstone-cluster

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "devops-capstone-cluster"
+  cluster_version = "1.29"
+
+  # Public access (for your local terminal) restricted by AWS IAM
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 2
+      instance_types = ["t3.medium"]
+      
+      # Attaching the SG we defined in security_groups.tf
+      vpc_security_group_ids = [aws_security_group.eks_nodes.id]
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
