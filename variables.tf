43f478ad5c76eda13b66603635b75a39a0d6d23e
Karan Patel <patel_karan_cool@hotmail.com>
2026-04-14 14:31:40 -0400

    Define variables for AWS infrastructure setup
    Add variables for AWS infrastructure configuration including VPC, subnets, and EKS cluster settings.

---
+++ variables.tf
@@ -0,0 +1,75 @@
+variable "region" {
+  description = "The AWS region to deploy infrastructure into"
+  type        = string
+  default     = "us-east-1"
+}
+
+# --- Networking Variables ---
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "devops-capstone-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones for the VPC"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "private_subnets" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnets" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+# --- EKS Variables ---
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "devops-capstone-cluster"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.29"
+}
+
+variable "node_instance_types" {
+  description = "EC2 instance types for the EKS node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
